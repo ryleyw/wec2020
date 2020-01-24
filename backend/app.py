39638969609d4476b8249c561ab9f99213fbbe8d
malcolmseyd <malcolmseyd@gmail.com>
2020-01-24 11:19:14 -0600

    add new transaction and fix CORS

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import csv
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 # @app.route('/user/<username>')
 # def gethistory(username):
@@ -18,8 +20,8 @@ app = Flask(__name__)
 def getuserjson():
     user = request.json['user']
     if user.lower() == "karen":
-        chequing_data = csv_to_json(f"db/LEVEL_1/CHEQUING.csv")
-        savings_data = csv_to_json(f"db/LEVEL_1/SAVINGS.csv")
+        chequing_data = read_json("db/LEVEL_1/CHEQUING.json")
+        savings_data = read_json("db/LEVEL_1/SAVINGS.json")
 
         chequing_total = get_dict_total(chequing_data)
         savings_total = get_dict_total(savings_data)
@@ -42,24 +44,59 @@ def getuserjson():
     return "404: User not found!"
 
 
+@app.route('/newtransaction', methods=['POST'])
+def newtransaction():
+    filename = "db/LEVEL_1/SAVINGS.json"
+    user = request.json['user']
+    transaction = {
+        "Date": request.json['Date'],
+        "Type": request.json['Type'],
+        "Amount": request.json['Amount'],
+        "Title": request.json['Title'],
+    }
+    json_data = {
+        "user": user,
+        "transaction": transaction
+    }
+
+    transaction_history = read_json(filename)
+    transaction_history.append(transaction)
+    write_json(transaction_history, filename)
+
+    return str(read_json(filename))
+
+
 @app.route('/')
 def index_page():
     return '<a href="/user/karen?db=CHEQUING"> Karen\'s JSON</a>'
 
 def csv_to_json(filename):
     csv_file = open(filename, "r")
-    #json_file = open(filename.split(".")[0]+".json", "w")
+    json_file = open(filename.split(".")[0]+".json", "w")
 
     fields = ("Date", "Type", "Amount", "Title")
     csv_data = csv.DictReader(csv_file, fields)
     next(csv_data)
 
-    json_str = []
+    json_list = []
 
     for transaction in csv_data:
-        json_str.append(transaction)
+        json_list.append(transaction)
 
-    return json_str
+    json.dump(json_list, json_file)
+    #return json_list
+
+
+def read_json(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+        #print(data)
+        return data
+
+
+def write_json(dict, filename):
+    with open(filename, "w") as f:
+        json.dump(dict, f)
 
 
 def get_dict_total(dict):
@@ -75,12 +112,13 @@ def get_dict_total(dict):
 
 
 def main():
-    savings_dict = csv_to_json("SAVINGS.csv")
-    chequing_dict = csv_to_json("CHEQUING.csv")
 
+    csv_to_json(f"db/LEVEL_1/SAVINGS.csv")
+    csv_to_json(f"db/LEVEL_1/CHEQUING.csv")
+
+    app.run()
 
 
 
 if __name__ == '__main__':
-    app.run()
-    #main()
+    main()
